e2241bfd8af5075ee202ce220f9dd5dda70e71d3
Carlos Santillan <csantill@gmail.com>
2020-07-16 00:11:15 -0500

    updtes\

--- News/app.py
+++ News/app.py
@@ -26,6 +26,9 @@ region_name = "us-east-2"
 access_key =  os.environ.get('ACCESS_KEY', '')
 secret_key =  os.environ.get('SECRET_KEY', '')
 
+print(f'a {access_key}')
+print(f'b {secret_key}')
+
 session = boto3.session.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region_name)
 client = session.client('secretsmanager')
 secret_value = client.get_secret_value(SecretId=secret_name)
