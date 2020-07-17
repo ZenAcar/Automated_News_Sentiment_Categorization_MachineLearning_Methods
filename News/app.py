39fe66378529c7117d47145d194c0cc45662fa05
Carlos Santillan <csantill@gmail.com>
2020-07-17 13:15:27 -0500

    Updates

--- News/app.py
+++ News/app.py
@@ -28,7 +28,7 @@ print(sys.path)
 
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('AWS_DATABASE_URL', '')
 # # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -39,13 +39,15 @@ class sentiment_results(db.Model):
     """Data model for user accounts."""
 
     __tablename__ = 'sentiment_results'
-    id = db.Column(db.Integer, primary_key=True   )
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(64), index=False,unique=True, nullable=False )
     title = db.Column(db.String(64), index=False,unique=True, nullable=False )
+    description = db.Column(db.String(64), index=False,unique=True, nullable=False )
     url = db.Column(db.String(64), index=False,unique=True, nullable=False )
     urlToImage    = db.Column(db.String(64), index=False,unique=True, nullable=False )
     publishedAt = db.Column(db.DateTime, index=False,unique=True, nullable=False )
-    articleSummary    = db.Column(db.String(64), index=False,unique=True, nullable=False )
-    articleSentiment    = db.Column(db.String(200), index=False,unique=True, nullable=False )
+    articleSummary    = db.Column(db.String(200), index=False,unique=True, nullable=False )
+    articleSentiment    = db.Column(db.String(64), index=False,unique=True, nullable=False )
     category    = db.Column(db.String(64), index=False,unique=True, nullable=False )
 # # from .models import census
 # try:
